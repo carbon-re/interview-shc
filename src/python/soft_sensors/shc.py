6155f5ea6a358567c77c2e9491f5e62a2a2e3f98
Bob Gregory <bob@carbonre.com>
2025-03-07 11:04:58 +0000

    Add pyright

---
+++ src/python/soft_sensors/shc.py
@@ -0,0 +1,8 @@
+import pandas as pd
+class ShcSoftSensor:
+
+    def transform(self, raw_data: pd.DataFrame) -> pd.DataFrame:
+        return raw_data
+
+    def calculate(self, transformed_data: pd.DataFrame) -> pd.DataFrame:
+        return transformed_data
---
+++ src/python/soft_sensors/test_shc_soft_sensor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import typing
+
+from python.soft_sensors import SoftSensor
+
+
+if typing.TYPE_CHECKING:
+    x: SoftSensor = ShcSoftSensor()
