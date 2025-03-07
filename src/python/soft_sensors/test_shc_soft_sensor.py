b12a6e5c03584dde716513b82a028db59af6b679
Bob Gregory <bob@carbonre.com>
2025-03-07 11:10:00 +0000

    Add first failing test

--- src/python/soft_sensors/test_shc_soft_sensor.py
+++ src/python/soft_sensors/test_shc_soft_sensor.py
@@ -2,8 +2,28 @@
 
 import typing
 
-from python.soft_sensors import SoftSensor
+import pandas as pd
 
+from src.python.soft_sensors import SoftSensor
+from src.python.soft_sensors.shc import ShcSoftSensor
+
+def test_shc():
+    data = {
+        'timestamp': ['2023-01-01 00:00:00'],
+        's_ph_sil_tput': [160.0],
+        'f_k_coal_tput': [12.9],
+        'f_k_coal_ncv': [6000],
+    }
+
+    df = pd.DataFrame(data)
+
+    sensor = ShcSoftSensor()
+    transformed = sensor.transform(df)
+    result = sensor.calculate(transformed)
+
+    expected = None
+
+    assert result.shc[0] == expected
 
 if typing.TYPE_CHECKING:
     x: SoftSensor = ShcSoftSensor()
