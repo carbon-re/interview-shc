0f9c56ca07da9e0bfd5d187f7572453ccedf239d
Bob Gregory <bob@carbonre.com>
2025-03-08 21:24:24 +0000

    passed def

--- src/python/soft_sensors/shc.py
+++ src/python/soft_sensors/shc.py
@@ -24,3 +24,15 @@ class CdeShcSoftSensor(ShcSoftSensor):
         raw_data["clinker"] = (raw_data["s_ph_sil_tput"] * 1000) / 1.55
         raw_data["energy"] = (raw_data["f_k_coal_tput"] * 1000 * raw_data["f_k_coal_ncv"]) + (raw_data["f_k_rdf_tput"] * 1000 * raw_data["f_k_rdf_ncv"])
         return raw_data[raw_data["s_ph_sil_tput"] > 10]
+
+
+class DefShcSoftSensor(ShcSoftSensor):
+
+    CLINKER_RATIO = 1.55
+    GJ_CONVERSION = 239
+
+    def transform(self, raw_data: pd.DataFrame) -> pd.DataFrame:
+        print(raw_data)
+        raw_data["clinker"] = (raw_data["s_ph_sil_tput"] * 1000) / 1.55
+        raw_data["energy"] = (raw_data["f_k_petcoke_tput"] * 1000 * raw_data["f_k_petcoke_ncv"]) + (raw_data["f_k_rdf_tput"] * 239000 * raw_data["f_k_rdf_ncv"])
+        return raw_data[raw_data["s_ph_sil_tput"] > 10]
