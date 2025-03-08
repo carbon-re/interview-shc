912996e575909c915d4fa7da2a44dd478aa33b88
Bob Gregory <bob@carbonre.com>
2025-03-08 21:35:50 +0000

    Comment

--- src/python/conftest.py
+++ src/python/conftest.py
@@ -5,6 +5,10 @@ import pytest
 
 @pytest.fixture
 def test_data():
+    """
+    This fixture will allow a test to load any csv file in the
+    test data directory of soft_sensors.
+    """
     def _(filename: str) -> pd.DataFrame:
         data_path = impresources.files(__package__) / "soft_sensors/test_data" / filename
         return pd.read_csv(str(data_path))
