7fed9328917519a1571f8e1a4eead3f9573b1219
Bob Gregory <bob@carbonre.com>
2025-03-08 18:12:24 +0000

    Hacks

--- src/python/soft_sensors/handler.py
+++ src/python/soft_sensors/handler.py
@@ -7,7 +7,7 @@ def handle(event, context):
     bucket = os.environ["BUCKET"]
     plant = event["plant"]
     data = wr.s3.read_csv(f's3://{bucket}/{plant}.csv')
-    soft_sensor = shc.ShcSoftSensor()
+    soft_sensor = shc.CdeShcSoftSensor() if plant == "cde" else shc.ShcSoftSensor()
     data = soft_sensor.transform(data)
 
     result = soft_sensor.calculate(data)
--- src/python/soft_sensors/shc.py
+++ src/python/soft_sensors/shc.py
@@ -6,13 +6,21 @@ class ShcSoftSensor:
     CLINKER_RATIO = 1.55
 
     def transform(self, raw_data: pd.DataFrame) -> pd.DataFrame:
-        print(raw_data)
         raw_data["f_k_coal_ncv"] = raw_data["f_k_coal_ncv"].ffill()
+
+        raw_data["clinker"] = (raw_data["s_ph_sil_tput"] * 1000) / 1.55
+        raw_data["energy"] = raw_data["f_k_coal_tput"] * 1000 * raw_data["f_k_coal_ncv"]
         return raw_data[raw_data["s_ph_sil_tput"] > 10]
 
     def calculate(self, transformed_data: pd.DataFrame) -> pd.DataFrame:
-        print(transformed_data)
-        transformed_data["clinker"] = (transformed_data["s_ph_sil_tput"] * 1000) / 1.55
-        transformed_data["energy"] = transformed_data["f_k_coal_tput"] * 1000 * transformed_data["f_k_coal_ncv"]
         transformed_data["shc"] = np.floor(transformed_data["energy"] / transformed_data["clinker"]).astype(int)
         return transformed_data[["timestamp", "shc"]]
+
+class CdeShcSoftSensor(ShcSoftSensor):
+
+    CLINKER_RATIO = 1.55
+
+    def transform(self, raw_data: pd.DataFrame) -> pd.DataFrame:
+        raw_data["clinker"] = (raw_data["s_ph_sil_tput"] * 1000) / 1.55
+        raw_data["energy"] = (raw_data["f_k_coal_tput"] * 1000 * raw_data["f_k_coal_ncv"]) + (raw_data["f_k_rdf_tput"] * 1000 * raw_data["f_k_rdf_ncv"])
+        return raw_data[raw_data["s_ph_sil_tput"] > 10]
