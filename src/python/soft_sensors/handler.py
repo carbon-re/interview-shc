e995de424d19cc580d04136e8f3f8c9c67aa9af3
Bob Gregory <bob@carbonre.com>
2025-03-08 08:10:25 +0000

    Switch to wrangler

--- src/python/soft_sensors/handler.py
+++ src/python/soft_sensors/handler.py
@@ -1,10 +1,10 @@
-import pandas as pd
+import awswrangler as wr
 from src.python.soft_sensors import shc
 
 import os
 
 def handle(event, context):
     bucket = os.environ["BUCKET"]
-    data = pd.read_csv(f's3://{bucket}/abc.csv')
+    data = wr.s3.read_csv(f's3://{bucket}/abc.csv')
     soft_sensor = shc.ShcSoftSensor()
     return soft_sensor.calculate(data)
