4da02120a2ed502d2ec5341c00db6df851496075
Sidney Ofosu <sidney@carbonre.com>
2025-03-19 15:06:37 +0000

    19-03-2025-Kitty-Kovacs

--- src/python/soft_sensors/shc.py
+++ src/python/soft_sensors/shc.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import numpy as np
 class ShcSoftSensor:
 
     def transform(self, raw_data: pd.DataFrame) -> pd.DataFrame:
+        raw_data["heat"] = raw_data["f_k_coal_tput"]*raw_data["f_k_coal_ncv"]
+        raw_data["mass_of_clinker"] = raw_data["s_ph_sil_tput"]/1.55
         return raw_data
 
     def calculate(self, transformed_data: pd.DataFrame) -> pd.DataFrame:
+        transformed_data["shc"] = transformed_data["heat"]/transformed_data["mass_of_clinker"]
+        transformed_data["shc"] = np.floor(transformed_data["shc"]).astype(int)
         return transformed_data
--- src/python/soft_sensors/test_shc_soft_sensor.py
+++ src/python/soft_sensors/test_shc_soft_sensor.py
@@ -10,9 +10,9 @@ from src.python.soft_sensors.shc import ShcSoftSensor
 def test_shc():
     data = {
         'timestamp': ['2023-01-01 00:00:00'],
-        's_ph_sil_tput': [160.0],
-        'f_k_coal_tput': [12.9],
-        'f_k_coal_ncv': [6000],
+        's_ph_sil_tput': [160.0], # mass of raw meal
+        'f_k_coal_tput': [12.9], # mass of coal 
+        'f_k_coal_ncv': [6000], # net calorific value
     }
 
     df = pd.DataFrame(data)
@@ -21,15 +21,25 @@ def test_shc():
     transformed = sensor.transform(df)
     result = sensor.calculate(transformed)
 
-    expected = None
+    expected = 749
 
     assert result.shc[0] == expected
 
 
 def test_shc_from_file(test_data):
     df = test_data("abc.csv")
-    print(df)
-    assert False
+    sensor = ShcSoftSensor()
+    transformed = sensor.transform(df)
+    result = sensor.calculate(transformed)
+
+    expected_json = {"2023-01-01 00:00:00": 746, 
+                     "2023-01-01 01:00:00": 764,
+                     }
+    
+    for date, expected_shc in expected_json.items():
+
+        actual_shc = result.loc[result['timestamp'] == date, 'shc'].values
+        assert actual_shc == expected_shc
 
 if typing.TYPE_CHECKING:
     x: SoftSensor = ShcSoftSensor()
