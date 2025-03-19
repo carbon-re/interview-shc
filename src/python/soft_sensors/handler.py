dd5331fc493f97a41b9e5865d2190ab027a88e58
Sidney Ofosu <sidney@carbonre.com>
2025-03-19 15:20:35 +0000

    Added Lizzy's Work

--- src/python/soft_sensors/handler.py
+++ src/python/soft_sensors/handler.py
@@ -1,7 +1,32 @@
-from src.python.soft_sensors import SoftSensor
+from src.python.soft_sensors.shc import ShcSoftSensor
+import awswrangler
+import pandas as pd
 
-def handle(event, context):
-    if event["plant"] == "hello":
-        return {
-            "message": "Hello, world"
-        }
+BUCKET_PATH = "s3://cre-data-shc20250319110005411800000001/abc.csv"
+
+# AWS Rangler
+# URI = s3://cre-data-shc20250319110005411800000001/abc.csv
+def handle(event: dict, context):
+
+    data = awswrangler.s3.read_csv(path=BUCKET_PATH)
+
+    data_dict = data_processing(data)
+
+    return data_dict
+
+    # if event["plant"] == "hello":
+    #     return {
+    #         "message": "Hello, world"
+    #     }
+
+def data_processing(data: pd.DataFrame):
+    sensor = ShcSoftSensor()
+
+    transformed = sensor.transform(data)
+    tranformed_df = sensor.calculate(transformed)
+
+    data_dict = {}
+    for _, row in tranformed_df.iterrows():
+        data_dict[row["timestamp"]] = row["shc"]
+
+    return data_dict
--- src/python/soft_sensors/test_shc_soft_sensor.py
+++ src/python/soft_sensors/test_shc_soft_sensor.py
@@ -7,12 +7,13 @@ import pandas as pd
 from src.python.soft_sensors import SoftSensor
 from src.python.soft_sensors.shc import ShcSoftSensor
 
+# 
 def test_shc():
     data = {
         'timestamp': ['2023-01-01 00:00:00'],
-        's_ph_sil_tput': [160.0],
-        'f_k_coal_tput': [12.9],
-        'f_k_coal_ncv': [6000],
+        's_ph_sil_tput': [160.0],  # tonnes / hour (clinker)
+        'f_k_coal_tput': [12.9],  # tonnes / hour (coal in kiln)
+        'f_k_coal_ncv': [6000],  # net calorific output (coal)
     }
 
     df = pd.DataFrame(data)
@@ -21,15 +22,25 @@ def test_shc():
     transformed = sensor.transform(df)
     result = sensor.calculate(transformed)
 
-    expected = None
+    expected = 749
 
-    assert result.shc[0] == expected
+    assert result['shc'][0] == expected
 
 
 def test_shc_from_file(test_data):
+    sensor = ShcSoftSensor()
+
     df = test_data("abc.csv")
     print(df)
-    assert False
+
+    transformed = sensor.transform(df)
+    result = sensor.calculate(transformed)
+
+    result = df.loc[df["timestamp"] == "2023-01-01 10:00:00"]['shc'].values
+
+    expected_result = 723
+    
+    assert result == expected_result
 
 if typing.TYPE_CHECKING:
     x: SoftSensor = ShcSoftSensor()
