2855f5e82e69b03cfe72912bb8c0b88cdf44af4a
Will Beaufoy <will@carbonre.com>
2025-03-21 13:21:56 +0000

    Last bit

--- src/python/soft_sensors/handler.py
+++ src/python/soft_sensors/handler.py
@@ -1,7 +1,17 @@
-from src.python.soft_sensors import SoftSensor
+import awswrangler as wr
+
+from src.python.soft_sensors.shc import ShcSoftSensor
+
 
 def handle(event, context):
-    if event["plant"] == "hello":
-        return {
-            "message": "Hello, world"
-        }
+    if event["plant"] == "abc":
+        data = wr.read_csv(path="s3://cre-data-shc20250321113614864500000001/abc.csv")
+
+        sensor = ShcSoftSensor()
+        transformed = sensor.transform(data)
+        result = sensor.calculate(transformed)
+
+        result.set_index("timestamp")
+        result.to_dict("index")
+
+        return result
