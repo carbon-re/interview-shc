969196f2192829d70787f2725e3683998ec3ed60
Will Beaufoy <will@carbonre.com>
2025-03-21 12:04:35 +0000

    Samantha WIP

--- src/python/soft_sensors/shc.py
+++ src/python/soft_sensors/shc.py
@@ -1,8 +1,41 @@
+import numpy as np
 import pandas as pd
-class ShcSoftSensor:
 
+
+class ShcSoftSensor:
     def transform(self, raw_data: pd.DataFrame) -> pd.DataFrame:
-        return raw_data
+        raw_meal = raw_data.s_ph_sil_tput
+        clinker_ratio = 1.55
+        clinker_mass = raw_meal / clinker_ratio
+
+        fuel_mass = raw_data.f_k_coal_tput
+        calorific_value = raw_data.f_k_coal_ncv
+        heat = fuel_mass * calorific_value
+
+        timestamp = raw_data.timestamp
+
+        transformed_dictionary = {
+            "timestamp": timestamp,
+            "heat": heat,
+            "clinker_mass": clinker_mass,
+        }
+        transformed_data = pd.DataFrame(transformed_dictionary)
+
+        return transformed_data
 
     def calculate(self, transformed_data: pd.DataFrame) -> pd.DataFrame:
-        return transformed_data
+        heat = transformed_data.heat
+        clinker_mass = transformed_data.clinker_mass
+        heat_consumption = heat / clinker_mass
+
+        rounded_heat_consumption = heat_consumption.apply(np.floor).astype(int)
+
+        timestamp = transformed_data.timestamp
+
+        heat_consumption_dictionary = {
+            "timestamp": timestamp,
+            "shc": rounded_heat_consumption,
+        }
+        heat_consumption_data = pd.DataFrame(heat_consumption_dictionary)
+
+        return heat_consumption_data
--- src/python/soft_sensors/test_shc_soft_sensor.py
+++ src/python/soft_sensors/test_shc_soft_sensor.py
@@ -3,16 +3,59 @@
 import typing
 
 import pandas as pd
+from pandas.testing import assert_frame_equal
 
 from src.python.soft_sensors import SoftSensor
 from src.python.soft_sensors.shc import ShcSoftSensor
 
+
+def test_transform():
+    data = {
+        "timestamp": ["2023-01-01 00:00:00"],
+        "s_ph_sil_tput": [160.0],
+        "f_k_coal_tput": [12.9],
+        "f_k_coal_ncv": [6000],
+    }
+
+    df = pd.DataFrame(data)
+
+    sensor = ShcSoftSensor()
+    transformed = sensor.transform(df)
+
+    expected_dictionary = {
+        "timestamp": ["2023-01-01 00:00:00"],
+        "heat": [77400.0],
+        "clinker_mass": [103.2258064516129],
+    }
+    expected_result = pd.DataFrame(expected_dictionary)
+
+    assert_frame_equal(transformed, expected_result)
+
+
+def test_calculate():
+    data = {
+        "timestamp": ["2023-01-01 00:00:00"],
+        "heat": [77400.0],
+        "clinker_mass": [103.2258064516129],
+    }
+
+    df = pd.DataFrame(data)
+
+    sensor = ShcSoftSensor()
+    calculated = sensor.calculate(df)
+
+    expected_dictionary = {"timestamp": ["2023-01-01 00:00:00"], "shc": [749]}
+    expected_result = pd.DataFrame(expected_dictionary)
+
+    assert_frame_equal(calculated, expected_result)
+
+
 def test_shc():
     data = {
-        'timestamp': ['2023-01-01 00:00:00'],
-        's_ph_sil_tput': [160.0],
-        'f_k_coal_tput': [12.9],
-        'f_k_coal_ncv': [6000],
+        "timestamp": ["2023-01-01 00:00:00"],
+        "s_ph_sil_tput": [160.0],
+        "f_k_coal_tput": [12.9],
+        "f_k_coal_ncv": [6000],
     }
 
     df = pd.DataFrame(data)
@@ -21,15 +64,26 @@ def test_shc():
     transformed = sensor.transform(df)
     result = sensor.calculate(transformed)
 
-    expected = None
+    expected = 749
 
     assert result.shc[0] == expected
 
 
 def test_shc_from_file(test_data):
     df = test_data("abc.csv")
-    print(df)
-    assert False
+
+    sensor = ShcSoftSensor()
+    transformed = sensor.transform(df)
+    resulting_dataframe = sensor.calculate(transformed)
+
+    expected_dictionary = {
+        "timestamp": ["2023-01-01 00:00:00", "2023-01-01 01:00:00"],
+        "shc": [746, 764],
+    }
+    expected_result = pd.DataFrame(expected_dictionary)
+
+    assert_frame_equal(resulting_dataframe.head(2), expected_result)
+
 
 if typing.TYPE_CHECKING:
     x: SoftSensor = ShcSoftSensor()
